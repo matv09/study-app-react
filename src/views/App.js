c961ca10bfbaf55e4149e42257ee6482c54772ac
Mateusz <mateuszciszek09@gmail.com>
2022-05-27 18:36:21 +0200

    test

--- src/views/App.js
+++ src/views/App.js
@@ -12,7 +12,7 @@ function App() {
         <a
           className="App-lin k"
           href="https://reactjs.org"
-          target="_blank"
+          target="_blanssk"
           rel="noopener noreferrer"
         >
           Learn React
