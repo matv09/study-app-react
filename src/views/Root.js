afe7f5a54fdd07505b5b02467728249b8e94f187
Mateusz <mateuszciszek09@gmail.com>
2022-05-28 13:36:31 +0200

    v2

---
+++ src/views/Root.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import UsersList from 'components/UsersList/UsersList';
+
+const Root = () => (
+  <>
+    <UsersList />
+  </>
+);
+
+export default Root;
