5391f6f81e9ae52ff3f4f915fbb99dcfca45dff8
Mateusz <mateuszciszek09@gmail.com>
2022-05-31 00:21:21 +0200

    excs

--- src/components/molecules/UsersListItem/UsersListItem.js
+++ src/components/molecules/UsersListItem/UsersListItem.js
@@ -1,14 +1,17 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import Button from 'components/atoms/Button/Button';
+import { Wrapper } from './UsersListItem.styles';
 
-const UsersListItem = ({ userData: { name, average, attendance } }) => (
-  <li>
+const UsersListItem = ({ userData: { name, average, attendance = '0%' } }) => (
+  <Wrapper>
     <div>{average} </div>
     <div>
       <p>{name}</p>
       <p>{attendance}</p>
     </div>
-  </li>
+    <Button />
+  </Wrapper>
 );
 
 UsersListItem.propTypes = {
