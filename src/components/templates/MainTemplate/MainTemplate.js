57fe973c890150fd65d3a74368cba02c1b9da4a3
Mateusz <mateuszciszek09@gmail.com>
2022-07-08 20:50:40 +0200

    news v1

--- src/components/templates/MainTemplate/MainTemplate.js
+++ src/components/templates/MainTemplate/MainTemplate.js
@@ -1,12 +1,29 @@
 import React from 'react';
+import styled from 'styled-components';
 import { Wrapper } from 'components/templates/MainTemplate/MainTemplate.styles'
 import Navigation from 'components/organisms/Navigation/Navigation';
+import { SearchBar } from '../../organisms/SearchBar/SearchBar';
+
+
+const News = styled.div`
+  grid-row: 1 / 3;
+  grid-column: 3 / 3;
+  border-left: 1px solid ${({theme}) => theme.colors.darkPurple};
+`;
 
 const MainTemplate = ({ children }) => {
   return (
     <Wrapper>
       <Navigation />
+      <SearchBar />
       {children}
+      <News>
+        <p>Lorem ipsum</p>
+        <p>Lorem ipsum</p>
+        <p>Lorem ipsum</p>
+        <p>Lorem ipsum</p>
+        <p>Lorem ipsum</p>
+      </News>
     </Wrapper>
   );
 };
