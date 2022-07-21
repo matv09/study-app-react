4004d926aa1300088f60e2d31cb8c4bf96033228
Mateusz <mateuszciszek09@gmail.com>
2022-07-22 00:21:19 +0200

    storybook-final

---
+++ src/components/molecules/StudentsListItem/StudentsListItem.stories.js
@@ -0,0 +1,38 @@
+import StudentsListItem from './StudentsListItem';
+
+export default {
+  title: 'Components/Molecules/StudentsListItem',
+  component: StudentsListItem,
+}
+
+
+const Template = (args) => <StudentsListItem {...args} />;
+
+export const BadGrades = Template.bind({});
+BadGrades.args = {
+  userData: {
+    name: 'Adam Romański',
+    attendance: '55%',
+    average: '2.0',
+  }
+}
+
+
+export const MediumGrades = Template.bind({});
+MediumGrades.args = {
+  userData: {
+    name: 'Adam Romański',
+    attendance: '55%',
+    average: '3.5',
+  }
+}
+
+export const GoodGrades = Template.bind({});
+GoodGrades.args = {
+  userData: {
+    name: 'Adam Romański',
+    attendance: '55%',
+    average: '5.0',
+  }
+}
+
