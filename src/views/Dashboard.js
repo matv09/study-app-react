92bb920041de1c6eb933da5f527ff05fe923fa2a
Mateusz <mateuszciszek09@gmail.com>
2022-07-24 00:13:51 +0200

    msw-database-final

--- src/views/Dashboard.js
+++ src/views/Dashboard.js
@@ -9,29 +9,8 @@ import useModal from 'components/organisms/Modal/useModal';
 import { StyledAverage } from 'components/molecules/StudentsListItem/StudentsListItem.styles';
 import StudentDetails from 'components/molecules/StudentDetails/StudentDetails';
 import Modal from '../components/organisms/Modal/Modal';
+import axios from 'axios';
 
-const mockStudent = {
-  id: '1',
-  name: 'Adam Romański',
-  attendance: '39%',
-  average: '2.3',
-  group: 'A',
-  course: 'Business Philosophy',
-  grades: [
-    {
-      subject: 'Business Philosophy',
-      average: '3.3',
-    },
-    {
-      subject: 'Marketing',
-      average: '4.7',
-    },
-    {
-      subject: 'Modern Economy',
-      average: '2.5',
-    },
-  ],
-}
 
 const Dashboard = () => {
   const [groups, setGroups] = useState([]);
@@ -45,6 +24,8 @@ const Dashboard = () => {
       const groups = await getGroups();
       setGroups(groups);
     })();
+
+
   }, [getGroups]);
 
   const handleOpenStudentDetails = async (id) => {
@@ -70,7 +51,7 @@ const Dashboard = () => {
       <GroupWrapper>
         <StudentsList handleOpenStudentDetails={handleOpenStudentDetails} />
           <Modal isOpen={isOpen} handleClose={handleCloseModal}>
-            <StudentDetails student={mockStudent} />
+            <StudentDetails student={currentStudent} />
           </Modal>
       </GroupWrapper>
     </Wrapper>
